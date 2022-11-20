5ca9b22b22fcb3bb22ce253df9858bc2c024aa17
weward <dev.weward@gmail.com>
2022-11-20 17:05:08 +0800

    highlightjs

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
+        <select @change="changeStyle($event)" v-model="obj.styleId">
+            <option v-if="!obj.hasBackend" v-for="style in styleList" :value="style">{{ style }}</option>
+            <option v-if="obj.hasBackend" v-for="(style, id) in styleList" :value="id">{{ style }}</option>
+        </select>
+
         <textarea 
-            v-model="obj.code"
+            v-model.trim="obj.code"
             id="" 
             name="code" 
             cols="30" 
@@ -9,23 +14,70 @@
             style="width: 100%;">
         </textarea>
 
-        <highlightjs language='vue' :code="obj.code" />
+        <highlightjs 
+            language='vue' 
+            :code="obj.code" 
+            contenteditable="true" 
+            class="tabbed" 
+        />
 
     </div>
 </template>
 
 <script setup>
+
+    import { reactive, onMounted } from 'vue'
+    import { styleList } from '@/static/styleList'
+
+    import hljsVuePlugin from "@highlightjs/vue-plugin";
+
     import 'highlight.js/lib/common'
     import 'highlight.js/styles/stackoverflow-light.css'
-    
-    import { reactive, onMounted } from 'vue'
-    import hljsVuePlugin from "@highlightjs/vue-plugin";
-    
+
     const highlightjs = hljsVuePlugin.component
 
     const obj = reactive({
         code: '',
+        styleId: '',
+        hasBackend: import.meta.env.VITE_HAS_BACKEND,
+        chosenStyle: '../../css/atom-one-dark.css',
     })
+
+    async function changeStyle(event) {
+        this.obj.styleId = event.target.value
+        this.obj.chosenStyle = `${this.obj.styleId}.css`
+
+        await this.removeStyle()
+
+        await this.appendStyle()
+
+        // set store value - this.obj.chosenStyle
+        
+    }
+
+    function appendStyle() {
+        let assetUrl = this.getAssetUrl()
+
+        let style = document.createElement('link');
+        style.type = "text/css"
+        style.rel = "stylesheet"
+        style.id = "chosenStyle"
+        style.href = assetUrl
+
+        document.head.appendChild(style);
+    }
+
+    function getAssetUrl() {
+        return new URL(`../../public/css/${this.obj.chosenStyle}`, import.meta.url)
+    }
+
+    function removeStyle() {
+        let oldStyle = document.getElementById('chosenStyle')
+
+        if (oldStyle != null) {
+            oldStyle.remove()
+        }
+    }
 
     onMounted(() => {
         console.log(import.meta.env.VITE_APP_NAME)
@@ -34,6 +86,8 @@
 
 </script>
 
-<style>
-    
+<style scope>
+.tabbed {
+    tab-size: 4;
+}
 </style>
